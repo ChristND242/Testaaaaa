3f07d99b35df7e721004538b3d454c5930e04864
Christ ND <91074634+ChristND242@users.noreply.github.com>
2025-01-18 20:44:50 +0800

    Add files via upload

---
+++ scholarships.js
@@ -0,0 +1,141 @@
+var itemslist = [
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Computer Science', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Full Scolarship'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'Chinese', 'program_name': 'Business', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Full Scolarship'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Marketing', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Full Scolarship'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Partial Scolarship'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Partial Scolarship'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+    {'id': '42235', 'city': 'Shanghai', 'degree': 'Master', 'teaching_language': 'English', 'program_name': 'Mechanical Engineering', 'intake': '2023-01-01', 'deadline': '2023-01-01', 'type': 'Self-sponsored'},
+]
+
+var array = []
+var array_lenght = 0;
+var table_size = 5;
+var start_index = 1;
+var end_index = 0;
+var current_page = 1;
+var max_page = 0;
+
+function preLoadCalculations() {
+    searchTable()
+    
+    array_lenght = array.length
+    max_page = array_lenght / table_size
+
+    if ((array_lenght % table_size) > 0) {
+        max_page ++
+    }
+}
+
+function searchTable() {
+    var search_text = $("#search_text").val()
+    if (search_text != "") {
+        temp_array = itemslist.filter(function(object) {
+            return object.item.toString().includes(search_text)
+                || object.item.toUppercase().includes(search_text.toUppercase())
+                || object.item.toString().includes(search_text)
+                || object.item.toString().includes(search_text)
+
+        })
+        
+    }else{
+        array = itemslist
+    }
+    
+}
+
+function displayIndexButtons() {
+    preLoadCalculations()
+    $(".pagination_buttons button").remove()
+    $(".pagination_buttons").append('<button id="btn" onclick="prev()">Prev</button>')
+    for (var i = 1; i <= max_page; i++) {
+        $(".pagination_buttons").append('<button id="btn" onclick="indexPagination('+i+')" index="'+i+'">'+i+'</button>')
+    }
+
+
+    $(".pagination_buttons").append('<button id="btn" onclick="next()">Next</button>')
+    hightlightIndexButton()
+}
+
+function hightlightIndexButton() {
+    start_index = (current_page - 1) * table_size + 1
+    end_index = start_index + table_size - 1
+    if (end_index > array_lenght) {
+        end_index = array_lenght
+    }
+    $(".pagination span").text("Showing " + start_index + " to " + end_index + " of " + array_lenght + " entries")
+    $(".pagination_buttons button").removeClass("active")
+    $(".pagination_buttons button[index='"+current_page+"']").addClass("active") // highlight the current page
+
+    displaytablerows()
+}
+
+function displaytablerows() {
+    $(".container table tbody tr").remove()
+    var tab_start = start_index - 1
+    var tab_end = end_index
+    for (var i = tab_start; i < tab_end; i++) {
+        var tr = '<tr><td>'+array[i].id+'</td><td>'+array[i].city+'</td><td>'+array[i].degree+'</td><td>'+array[i].teaching_language+'</td><td>'+array[i].program_name+'</td><td>'+array[i].intake+'</td><td>'+array[i].deadline+'</td><td>'+array[i].type+'</td></tr>'
+        $(".container table tbody").append(tr)
+    }
+}
+
+displayIndexButtons()
+
+function next () {
+    if (current_page < max_page) {
+        current_page++
+        hightlightIndexButton()
+    }
+}
+
+function prev () {
+    if (current_page > 1) {
+        current_page--
+        hightlightIndexButton()
+    }
+}
+
+function indexPagination (index) {
+    current_page = parseInt(index)
+    hightlightIndexButton() 
+}
+
+$("#table_size").change(function(){ // when the user selects a new number of rows to display
+    table_size = parseInt($(this).val())
+    current_page = 1
+    start_index = 1
+    displayIndexButtons()
+})
+
+$("#search_btn").click(function(){
+    current_page = 1
+    start_index = 1
+    displayIndexButtons()
+})
